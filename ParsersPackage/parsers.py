20593c28e240317e1b8e7b105d956c7ebb1a57dc
MaksimovArtem <provadok@gmail.com>
2018-06-17 18:15:55 +0300

    Parses

--- ParsersPackage/parsers.py
+++ ParsersPackage/parsers.py
@@ -5,7 +5,8 @@ import re #regular expressions
 
 def get_html(url):
 	response = requests.get(url)
-	response.encoding = 'cp1251' # encode old site's format
+	if "htm" in url:
+		response.encoding = 'cp1251' # encode for htm and html web-pages
 	return response.text
 
 
@@ -19,21 +20,37 @@ def search_needed_news(string): #small dictionary that I used to get needed stri
 		return False
 
 
-def parser(html):
-	soup = BeautifulSoup(html, 'html.parser')
-	all_news = soup.find_all(text = re.compile("обрани"), limit = 3) # )))))))))))))))))
+def prettify_string(string):
+	result = list()
+	for char in string:
+			if char.isalnum():
+				result.append(char)
+			elif (char.isspace() or char in ".-:;") and (not result or not result[-1].isspace()):
+				result.append(char)
+	return ''.join(result)
 
-	if len(all_news)!=0:
-		print(len(all_news))
+def get_needed_lvl(tag, lvl):
+	for i in range(lvl):
+		tag = tag.parent
+		i+=1
+	return (str)(tag.get_text()).lower()
+
+
+def parser(html, lvl):
+	soup = BeautifulSoup(html, 'html.parser')
+	all_news = soup.find_all(text = re.compile("обрани"), limit = 3) # last news
+
+	#if len(all_news) != 0:
+	#	print(len(all_news))
 	#check
 
 	answer = ''
 	for news in all_news: 
-		string = (str)(news.parent.parent.parent.parent.get_text()).lower()
-		
+		#string = (str)(news.parent.parent.get_text()).lower()
+		string = get_needed_lvl(news,lvl)
 		if search_needed_news(string):
-			print(news.parent.parent.parent.parent.get_text())
-			answer = news.parent.parent.get_text() # 2nd lvl
+			print(prettify_string(string))
+			answer = string # 2nd lvl
 		else:
 			continue
 	return answer
--- main.py
+++ main.py
@@ -1,17 +1,17 @@
 import ParsersPackage.parsers
 
 schools_map = {
-	#'Школа 5' : 'http://www.nnovschool5.edusite.ru/p505aa1.html',
-	#'Школа 12': 'http://schoolnn12.narod.ru/'
-	'Школа 15': 'http://www.school-15.ru/parents.htm'
+	'Школа 5' : ['http://www.nnovschool5.edusite.ru/p505aa1.html', 2],
+	'Школа 12': ['http://schoolnn12.narod.ru/', 2],
+	'Школа 15': ['http://www.school-15.ru/parents.htm', 4]
 }
 
 news_map = dict()
 
 def main():
-	for url in schools_map.values():
-		html = ParsersPackage.parsers.get_html(url)
-		ParsersPackage.parsers.parser(html)
+	for iterator in schools_map.values():
+		html = ParsersPackage.parsers.get_html(iterator[0])
+		ParsersPackage.parsers.parser(html, iterator[1])
 
 
 
