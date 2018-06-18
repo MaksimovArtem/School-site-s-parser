4637bb6005f324b2ac54f0d9a9c8bed3a8542dce
MaksimovArtem <provadok@gmail.com>
2018-06-18 20:42:13 +0300

    csv reader

--- ParsersPackage/parsers.py
+++ ParsersPackage/parsers.py
@@ -13,7 +13,7 @@ def get_html(url):
 def search_needed_news(string): #small dictionary that I used to get needed string
 	text1 = "родител"
 	text2 = "первоклассник"
-	text3 = "остоится"
+	text3 = "cостоится"
 	if string.find(text1) != -1 or string.find(text2)!= -1 or string.find(text3)!= -1:
 		return True
 	else:
@@ -28,7 +28,7 @@ def prettify_string(string):#make string better)
 			elif (char.isspace() or char in ".-:;") and (not result or not result[-1].isspace()):
 				result.append(char)
 	return ''.join(result)
-
+	
 
 def get_needed_lvl(tag, lvl):#going up in tag tree to make parsing sentence more readable
 	for i in range(lvl):
@@ -41,12 +41,16 @@ def parser(html, lvl):
 	soup = BeautifulSoup(html, 'html.parser')
 	all_news = soup.find_all(text = re.compile("обрани"), limit = 3) # newest 3 words
 	answer = ''
-
-	for news in all_news: 
-		string = get_needed_lvl(news,lvl)
-		if search_needed_news(string):
-			answer = prettify_string(string)
-			print(answer)	
+	try:
+		for news in all_news: 
+			string = get_needed_lvl(news,lvl)
+			if search_needed_news(string):
+				answer += prettify_string(string)
+			else:
+				continue
+		if(len(answer) > 1500):
+			return "Something has broken"
 		else:
-			continue
-	return answer
+			return answer
+	except:
+		return ""	
--- main.py
+++ main.py
@@ -1,20 +1,19 @@
-import ParsersPackage.parsers
+from ParsersPackage.parsers import get_html, parser
 import csv
-
-schools_map = {
-	'Школа 5' : ['http://www.nnovschool5.edusite.ru/p505aa1.html', 2],
+from tqdm import tqdm
+#schools_map = {
+	#'Школа 5' : ['http://www.nnovschool5.edusite.ru/p505aa1.html', 2],
 	#'Школа 12': ['http://schoolnn12.narod.ru/', 2],
 	#'Школа 15': ['http://www.school-15.ru/parents.htm', 4]
-}
+#}
 
 news_map = dict()
-'''
+
 def csv_reader(file):
 
 	reader = csv.reader(file)
 	for row in reader:
 		string = " ".join(row)
-		print(string)
 		result_string = list()
 		i = j = 0 # i - key j - value of dictionary
 		for char in string:
@@ -33,24 +32,28 @@ def csv_reader(file):
 					word3 = "".join(result_string)
 					result_string = []
 		schools_map[word1] = [word2, word3]
-'''
+
+
+def csv_writer(file):
+	with open(file,"w", newline = '') as file:
+		writer = csv.writer(file, delimiter = ',')
+		for school in schools_map:
+			
 
 def main():
-	
-	#for i in schools_map.items():
-	#	print(i)
-	
-	for iterator in schools_map.items():
-		html = ParsersPackage.parsers.get_html(iterator[1][0]) #2nd element of tuple; 1 element of list
-		news_map[iterator[0]] = ParsersPackage.parsers.parser(html, (int)(iterator[1][1]))
 
+	#for iterator in tqdm(schools_map.items()):
+	for iterator in schools_map.items():
+		html = get_html(iterator[1][0]) #2nd element of tuple; 1 element of list
+		news_map[iterator[0]] = parser(html, (int)(iterator[1][1]))
+		
 	for i in news_map.items():
 		print(i)
 	
 
 if __name__ == '__main__':
-	#schools_map = dict()
-	#csv_path = "initial_data.csv"
-	#with open(csv_path, "r") as file:
-	#	csv_reader(file)
+	schools_map = dict()
+	csv_path = "initial_data.csv"
+	with open(csv_path, "r") as file:
+		csv_reader(file)
 	main()
